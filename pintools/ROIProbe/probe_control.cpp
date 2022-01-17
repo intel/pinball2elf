550559b3acaec10a5963a700c7339e90e6bb9a2e
Harish Patil <harish.patil@intel.com>
2022-01-17 14:52:03 -0500

    Renamed ROIperf->ROIProbe and added a generic probe_control.cpp tool.

---
+++ pintools/ROIProbe/probe_control.cpp
@@ -0,0 +1,71 @@
+/*BEGIN_LEGAL 
+BSD License 
+
+Copyright (c)2022 Intel Corporation. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+END_LEGAL */
+#include "pin.H"
+#include "probe_control.H"
+
+static PROBE_CONTROL::PROBECONTROL pbcontrol;
+using namespace std;
+
+INT32 Usage()
+{
+    cerr << KNOB_BASE::StringKnobSummary() << endl;
+
+    return -1;
+}
+
+VOID MyProbeHandler(PROBE_CONTROL::PROBE_EVENT_TYPE pe)
+{
+  cerr << "MyHandler called: ";
+  switch(pe)
+      {
+        case PROBE_CONTROL::PROBE_EVENT_START:
+        {
+          cerr <<  "start";
+          break;
+        }
+        case PROBE_CONTROL::PROBE_EVENT_STOP:
+        {
+          cerr <<  "stop";
+          break;
+        }
+        default:
+        {
+          cerr <<  "INVALID";
+        }
+      }
+  cerr << endl << flush;
+  return;
+}
+
+// argc, argv are the entire command line, including pin -t <toolname> -- ...
+int main(int argc, char * argv[])
+{
+    if( PIN_Init(argc,argv) )
+    {
+        return Usage();
+    }
+    PIN_InitSymbols();
+
+    if (!pbcontrol.Activate())
+    {
+      cerr << "WARNING: Probe control not activated." << endl;
+    }
+    pbcontrol.RegisterHandler(MyProbeHandler);
+    // Start the program, never returns
+    PIN_StartProgramProbed();
+
+    return 0;
+}
