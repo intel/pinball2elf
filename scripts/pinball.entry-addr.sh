8cc9033f1b4cd015eeda0158d426a9a30411d9ea
Harish Patil <harish.patil@intel.com>
2023-04-11 11:06:02 -0400

    scripts/pinball.entry-addr.sh : takes 'thread_number' as an optional argument.
    Usage: pinball.entry-addr.sh pinball_basename <thread_number>
    
    Example:
    .../pinball.entry-addr.sh pinball.mt/log_0 3
    Thread: 3 Entry Address: 0x2aaac0b517b1

--- scripts/pinball.entry-addr.sh
+++ scripts/pinball.entry-addr.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
-# Usage: pinball.entry-addr.sh pinball_basename
+# Usage: pinball.entry-addr.sh pinball_basename <thread_number>
 # Output: Application THREAD-0 entry PC
 # ./pinball.entry-addr.sh ../examples/MT/pinball.mt/log_0
 #0x2aaac07dbf88
+ERROR()
+{
+  echo " Usage: pinball.entry-addr.sh pinball_basename <thread_number> "
+  exit
+}
+
 endian()
 {
   v=$1
@@ -15,15 +21,30 @@ endian()
   echo
 }
 #set -x
+if  [ $# -lt 1 ];  then
+    ERROR
+fi
+
 PINBALL=$1
+TNO=0
+if  [ $# -eq 2 ];  then
+  TNO=$2
+fi
+
 RIPNO=`grep -i RIP $PINBALL.global.log | awk '{print $2}'`
-if [ -e  $PINBALL.0.reg ]; 
+if [ -e  $PINBALL.$TNO.reg ]; 
 then
-RIPValue=`cat $PINBALL.0.reg | grep -e "^$RIPNO" | head -1 | awk '{print $2}'`
+RIPValue=`cat $PINBALL.$TNO.reg | grep -e "^$RIPNO" | head -1 | awk '{print $2}'`
 fi
-if [ -e  $PINBALL.0.reg.bz2 ]; 
+if [ -e  $PINBALL.$TNO.reg.bz2 ]; 
 then
-RIPValue=`bzcat $PINBALL.0.reg.bz2 | grep -e "^$RIPNO" | head -1 | awk '{print $2}'`
+RIPValue=`bzcat $PINBALL.$TNO.reg.bz2 | grep -e "^$RIPNO" | head -1 | awk '{print $2}'`
 fi
-echo -n "0x"
+
+if [ -z $RIPValue ];
+then
+  echo "Thread $TNO not valid"
+  ERROR
+fi
+echo -n "Thread: $TNO Entry Address: 0x"
 endian $RIPValue
